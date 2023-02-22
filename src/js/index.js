e844294fcb45f9212a7a7eda8157f9ed27a28d58
Woodiik <vovikvova04@gmail.com>
2023-02-22 19:28:56 +0200

    change styles

--- src/js/index.js
+++ src/js/index.js
@@ -130,3 +130,13 @@ function renderCards({
 </div>`;
   refs.gallery.insertAdjacentHTML('beforeend', markup);
 }
+function scroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
